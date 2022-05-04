964f2af039b0bda5e8d5db655c5df8f83323b0a9
notomo <notomo.motono@gmail.com>
2022-05-04 15:22:26 +0900

    Use test helper.cleanup()

--- lua/genvdoc/lib/testlib/helper.lua
+++ lua/genvdoc/lib/testlib/helper.lua
@@ -1,48 +1,44 @@
 local plugin_name = vim.split((...):gsub("%.", "/"), "/", true)[1]
-local M = require("vusted.helper")
+local helper = require("vusted.helper")
 
-M.root = M.find_plugin_root(plugin_name)
-M.test_data_path = "spec/test_data/"
-M.test_data_dir = M.root .. "/" .. M.test_data_path
+helper.root = helper.find_plugin_root(plugin_name)
+helper.test_data_path = "spec/test_data/"
+helper.test_data_dir = helper.root .. "/" .. helper.test_data_path
 local runtimepath = vim.o.runtimepath
 
-function M.before_each()
-  M.new_directory("")
-  vim.api.nvim_set_current_dir(M.test_data_dir)
+function helper.before_each()
+  helper.new_directory("")
+  vim.api.nvim_set_current_dir(helper.test_data_dir)
   vim.o.runtimepath = runtimepath
-  M.use_parsers()
+  helper.use_parsers()
 end
 
-function M.after_each()
-  vim.cmd("tabedit")
-  vim.cmd("tabonly!")
-  vim.cmd("silent! %bwipeout!")
+function helper.after_each()
+  vim.api.nvim_set_current_dir(helper.root)
+  helper.cleanup()
+  helper.cleanup_loaded_modules(plugin_name)
+  helper.delete("")
   print(" ")
-
-  vim.api.nvim_set_current_dir(M.root)
-
-  M.cleanup_loaded_modules(plugin_name)
-  M.delete("")
 end
 
-function M.use_parsers()
-  vim.o.runtimepath = M.root .. "/script/nvim-treesitter," .. vim.o.runtimepath
+function helper.use_parsers()
+  vim.o.runtimepath = helper.root .. "/script/nvim-treesitter," .. vim.o.runtimepath
 end
 
-function M.new_file(path, ...)
-  local f = io.open(M.test_data_dir .. path, "w")
+function helper.new_file(path, ...)
+  local f = io.open(helper.test_data_dir .. path, "w")
   for _, line in ipairs({ ... }) do
     f:write(line .. "\n")
   end
   f:close()
 end
 
-function M.new_directory(path)
-  vim.fn.mkdir(M.test_data_dir .. path, "p")
+function helper.new_directory(path)
+  vim.fn.mkdir(helper.test_data_dir .. path, "p")
 end
 
-function M.delete(path)
-  vim.fn.delete(M.test_data_dir .. path, "rf")
+function helper.delete(path)
+  vim.fn.delete(helper.test_data_dir .. path, "rf")
 end
 
 local asserts = require("vusted.assert").asserts
@@ -54,4 +50,4 @@ asserts.create("content"):register_eq(function(file_path)
   return "\n" .. expected
 end)
 
-return M
+return helper
